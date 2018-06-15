baedee511a8808de40dc035c3292a215acb9e1fe
naveen kumar <naveenv.vanamala@gmail.com>
2018-06-15 16:49:27 +0530

    Accepts the mobile number

--- accounts.cpp
+++ accounts.cpp
@@ -5,7 +5,9 @@ Account Information System :
 This is used to make the records of the customers of a Bank. This performs operations like storing data,
 displaying the stored data, modifying the data and so on.
 
-done by : V. Naveen Kumar(1601-15-733-301) and G. Sai Krishna(1601-15-733-303)
+This application is used by the bank employee form makin records of the customers.
+
+done by : V. Naveen Kumar(1601-15-733-301) and G. Sai Krishna(1601-15-733-303).
 */
 
 #include<iostream>
@@ -19,6 +21,7 @@ class account_info
         char account_number[20];
         char firstName[10];
         char lastName[10];
+        char mobile_no[10];
         double total_Balance;
     public:
         void read();
@@ -30,6 +33,7 @@ class account_info
         void edit();
 };
 
+//to get the details from the customers
 void account_info::read()
 {
 
@@ -39,42 +43,52 @@ void account_info::read()
     cin>>firstName;
     cout<<"Enter Last Name: ";
     cin>>lastName;
+    cout<<"enter mobile no. : ";
+    cin>>mobile_no;
     cout<<"Enter Balance: ";
     cin>>total_Balance;
     cout<<endl;
 }
+
+//to display the details of the customers
 void account_info::show()
 {
-    cout<<"\nAccount Number: "<<account_number<<endl;
-    cout<<"First Name: "<<firstName<<endl;
-    cout<<"Last Name: "<<lastName<<endl;
-    cout<<"Current Balance: Rs.  "<<total_Balance<<endl;
+    cout<<"\nAccount Number    : "<<account_number<<endl;
+    cout<<"First Name        : "<<firstName<<endl;
+    cout<<"Last Name         : "<<lastName<<endl;
+    cout<<"Mobile Number     : "<<mobile_no<<endl;
+    cout<<"Current Balance   : Rs.  "<<total_Balance<<endl;
     cout<<"-------------------------------"<<endl;
 }
+
+//to store the customer details into a file(here binary file is used to store)
 void account_info::write_file()
 {
     ofstream outfile;
-    outfile.open("acc_data", ios::binary|ios::app);
+    outfile.open("acc_data.acc", ios::binary|ios::app);
     read();
     outfile.write((char *)this, sizeof(*this));
     outfile.close();
 }
+
+//to display all customers details
 void account_info::read_file()
 {
     system("cls");
     int c=1;
     cout<<"\t\t*******************************************";
-    cout<<"\n\t\t\t\"Acount Information System\"\n"<<endl;
+    cout<<"\n\t\t\t\"Account Information System\"\n"<<endl;
     cout<<"\t\t*******************************************\n\n";
     ifstream infile;
-    infile.open("acc_data", ios::binary);
+    infile.open("acc_data.acc", ios::binary);
     if(!infile)
     {
         cout<<"\nError in Opening!\n File Not Found!!"<<endl;
         return;
     }
 
-    cout<<"\n\n****Data from file****\n"<<endl;
+
+    cout<<"\n\n****Data from file****\n\n"<<endl;
     while(!infile.eof())
     {
 
@@ -87,15 +101,17 @@ void account_info::read_file()
     }
     infile.close();
 }
+
+//to search a record based on the record number
 void account_info::search_()
 {
     system("cls");
     cout<<"\t\t\t*******************************************";
-    cout<<"\n\t\t\t\t\"Acount Information System\"\n"<<endl;
+    cout<<"\n\t\t\t\t\"Account Information System\"\n"<<endl;
     cout<<"\t\t\t*******************************************\n\n";
     int n;
     ifstream infile;
-    infile.open("acc_data", ios::binary);
+    infile.open("acc_data.acc", ios::binary);
     if(!infile)
     {
         cout<<"\nError in opening! File Not Found!!"<<endl;
@@ -116,15 +132,16 @@ void account_info::search_()
     show();
 }
 
+//to delete the customer details
 void account_info::del()
 {
     system("cls");
     cout<<"\t\t\t*******************************************";
-    cout<<"\n\t\t\t\t\"Acount Information System\"\n"<<endl;
+    cout<<"\n\t\t\t\t\"Account Information System\"\n"<<endl;
     cout<<"\t\t\t*******************************************\n";
     int n;
     ifstream infile;
-    infile.open("acc_data", ios::binary);
+    infile.open("acc_data.acc", ios::binary);
     if(!infile)
     {
         cout<<"\nError in opening! File Not Found!!"<<endl;
@@ -142,7 +159,7 @@ void account_info::del()
     cout<<"\n Enter Record Number to Delete: ";
     cin>>n;
     fstream tmpfile;
-    tmpfile.open("temp", ios::out|ios::binary);
+    tmpfile.open("temp.acc", ios::out|ios::binary);
     infile.seekg(0);
     for(int i=0; i<count; i++)
     {
@@ -153,20 +170,21 @@ void account_info::del()
     }
     infile.close();
     tmpfile.close();
-    remove("acc_data");
-    rename("temp", "acc_data");
+    remove("acc_data.acc");
+    rename("temp.acc", "acc_data.acc");
     }
 }
 
+//to modify the customer details
 void account_info::edit()
 {
     system("cls");
     cout<<"\t\t\t*******************************************";
-    cout<<"\n\t\t\t\t\"Acount Information System\"\n"<<endl;
+    cout<<"\n\t\t\t\t\"Account Information System\"\n"<<endl;
     cout<<"\t\t\t*******************************************\n\n";
     int n;
     fstream iofile;
-    iofile.open("acc_data", ios::in|ios::binary);
+    iofile.open("acc_data.acc", ios::in|ios::binary);
     if(!iofile)
     {
         cout<<"\nError in opening! File Not Found!!"<<endl;
@@ -176,7 +194,7 @@ void account_info::edit()
     int count = iofile.tellg()/sizeof(*this);
     if(count<=0)
     {
-        cout<<"there are no records in the file ";
+        cout<<"\nthere are no records in the file....... \n\n";
         return;
     }
     cout<<"\n There are "<<count<<" record in the file";
@@ -187,7 +205,7 @@ void account_info::edit()
     cout<<"Record "<<n<<" has following data"<<endl;
     show();
     iofile.close();
-    iofile.open("acc_data", ios::out|ios::in|ios::binary);
+    iofile.open("acc_data.acc", ios::out|ios::in|ios::binary);
     iofile.seekp((n-1)*sizeof(*this));
     cout<<"\nEnter data to Modify "<<endl;
     read();
@@ -198,7 +216,7 @@ int main()
     account_info A;
     int choice;
     cout<<"\t\t\t*******************************************";
-    cout<<"\n\t\t\t\t\"Acount Information System\"\n"<<endl;
+    cout<<"\n\t\t\t\t\"Account Information System\"\n"<<endl;
     cout<<"\t\t\t*******************************************\n";
     while(choice!=6)
     {
@@ -231,7 +249,7 @@ int main()
             case 6:
                 break;
             default:
-                cout<<"\nEnter corret choice(i.e, 1-6 only)\n\n";
+                cout<<"\nEnter corret option(i.e, 1-6 only)\n\n";
         }
     }
     return 0;
